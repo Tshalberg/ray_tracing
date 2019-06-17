1c0e639b598828a3f7d65d2a6bf24be3f2a32bf6
Thomas Halberg <thomashalberg@gmail.com>
2019-06-17 23:11:44 +0200

    chapter 12

--- cuda/src/main.cu
+++ cuda/src/main.cu
@@ -62,24 +62,87 @@ void free_world(hitable **d_list, hitable **d_world, camera **d_camera) {
     delete *d_camera;
 }
 
+// __device__
+// hitable *random_scene() {
+// 	int n = 500;
+// 	hitable **list = new hitable*[n+1];
+// 	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+// 	int i = 1;
+// 	for (int a=-11; a<11; a++) {
+// 		for (int b=-11; b<11; b++) {
+// 			float choose_mat = drand48();
+// 			vec3 center(a+0.9*drand48(), 0.2, b+0.9*drand48());
+// 			if ((center-vec3(4,0.2,0)).length() > 0.9) {
+// 				if (choose_mat < 0.8) { // diffuse (lambertian)
+// 					list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+// 				}
+// 				else if (choose_mat < 0.95) { //metal
+// 					list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+// 				}
+// 				else { // glass
+// 					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+// 	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+// 	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+// 	return new hitable_list(list,i);
+
+
+// __global__
+// void rand_init(curandState *rand_state){
+// 	if (threadIdx.x == 0 && blockIdx.x == 0) {
+// 	// Initialize a single random state
+// 	curand_init(1984, 0, 0, rand_state);
+// 	}
+// }
+
+#define RNG (curand_uniform(&local_rand_state))
+
 
 __global__
-void create_world(hitable **d_list, hitable **d_world, camera **d_camera, int nx, int ny) {
+void create_world(hitable **d_list, hitable **d_world, camera **d_camera, int nx, int ny, curandState *rand_state) {
 	if (threadIdx.x == 0 && blockIdx.x == 0) {
-		// *(d_list)   = new sphere(vec3(0, 0, -1), 0.5);
-		// *(d_list+1) = new sphere(vec3(0, -100.5, -1), 100);
-		*(d_list+0) = new sphere(vec3(0, 0, -1),      0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-		*(d_list+1) = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-		*(d_list+2) = new sphere(vec3(1, 0, -1),      0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
-		*(d_list+3) = new sphere(vec3(-1, 0, -1),     0.5, new dielectric(1.5));
-		*(d_list+4) = new sphere(vec3(-1, 0, -1),    -0.45, new dielectric(1.5));
-		*d_world    = new hitable_list(d_list,5);
 
-		vec3 lookfrom(-1,3,2);
-		vec3 lookat(0,0,-1);
-		float vfov = 30.0;
-		float dist_to_focus = (lookfrom-lookat).length();
+		curand_init(1337, 0, 0, &rand_state[0]);
+		curandState local_rand_state = rand_state[0];
+		float ran_num = RNG;
+
+		d_list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+		int i = 1;
+		for (int a=-11; a<11; a++) {
+			for (int b=-11; b<11; b++) {
+				float choose_mat = RNG;
+				vec3 center(a+0.9*RNG, 0.2, b+0.9*RNG);
+				if ((center-vec3(4,0.2,0)).length() > 0.9) {
+					if (choose_mat < 0.8) { // diffuse (lambertian)
+						d_list[i++] = new sphere(center, 0.2, new lambertian(vec3(RNG*RNG, RNG*RNG, RNG*RNG)));
+					}
+					else if (choose_mat < 0.95) { //metal
+						d_list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + RNG), 0.5*(1 + RNG), 0.5*(1 + RNG)), 0.5*RNG));
+					}
+					else { // glass
+						d_list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+					}
+				}
+			}
+		}
+
+		d_list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+		d_list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+		d_list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+		*d_world    = new hitable_list(d_list,i);
+
+		vec3 lookfrom(13,2,3);
+		vec3 lookat(0,0,0);
+		float dist_to_focus = 10.0;
 		float aperture = 0.1;
+		float vfov = 20.0;
 		*d_camera   = new camera(lookfrom, lookat, 
 								vec3(0, 1, 0), vfov, 
 								float(nx)/float(ny), 
@@ -138,17 +201,23 @@ int main() {
 	// Creating random states for GPU
 	curandState *d_rand_state;
 	checkCudaErrors(cudaMalloc((void **)& d_rand_state, num_pixels*sizeof(curandState)));
+	// Creating random state for create_world
+	curandState *d_rand_state_world;
+	checkCudaErrors(cudaMalloc((void **)& d_rand_state_world, sizeof(curandState)));
+
+
 
 	// Creating world
+	int num_obj = 22*22+3+1;
 	hitable **d_list;
-	checkCudaErrors(cudaMalloc((void **)& d_list, 5*sizeof(hitable *)));
+	checkCudaErrors(cudaMalloc((void **)& d_list, num_obj*sizeof(hitable *)));
 	hitable **d_world;
 	checkCudaErrors(cudaMalloc((void **)& d_world, sizeof(hitable *)));
 	// Creating camera
 	camera **d_camera;
 	checkCudaErrors(cudaMalloc((void **)& d_camera, sizeof(camera *)));
 
-	create_world<<<1,1>>>(d_list, d_world, d_camera, nx, ny);
+	create_world<<<1,1>>>(d_list, d_world, d_camera, nx, ny, d_rand_state_world);
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
 
